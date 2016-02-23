2f7b5dbd4f7324f63368a2d61516261e22d6c57c
Austin Fernandez <ryanaustinf@yahoo.com>
2016-02-23 23:39:33 +0800

    Maestro JAR

--- Driver.java
+++ Driver.java
@@ -3,24 +3,22 @@ import java.io.*;
 public class Driver {
 	public static void main(String[] args) {
 		try {
-			BufferedReader br = new BufferedReader(
-								new FileReader(
-									new File(args[0])));
-			String code = "";
-			String s;
-			do {
-				s = br.readLine();
-				if( s != null ) {
-					code += s + "\n";
-				}
-			} while(s != null);
+			// BufferedReader br = new BufferedReader(
+			// 					new FileReader(
+			// 						new File(args[0])));
+			// String code = "";
+			// String s;
+			// do {
+			// 	s = br.readLine();
+			// 	if( s != null ) {
+			// 		code += s + "\n";
+			// 	}
+			// } while(s != null);
 
-			br.close();
+			// br.close();
 
 			Interpreter interpreter 
-				= new Interpreter(new ConcreteTokenizer()
-									,ConcreteNonTerminalFactory.instance()
-									,code,"yourcfg.txt");
+				= new Interpreter(null,null,"",args[0]);
 			interpreter.interpret();
 		} catch(Exception e) {
 			// if( e.getMessage() != null) {
